6991c287c166f73ce6068b5ce4c14b3a8c312984
Jenny Swift <cheezyspaghetti@gmail.com>
2017-05-08 15:27:23 +1000

    Add page content

--- public/js/app.js
+++ public/js/app.js
@@ -12681,6 +12681,7 @@ module.exports = {
 //
 //
 //
+//
 
 module.exports = {
     data: function data() {
@@ -33980,7 +33981,7 @@ if (false) {
 module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
   return _vm._m(0)
 },staticRenderFns: [function (){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
-  return _c('div', [_c('h1', [_vm._v("Intraoral Camera")])])
+  return _c('div', [_c('h1', [_vm._v("Intraoral Camera")]), _vm._v(" "), _c('p', [_vm._v("The intraoral camera is wonderful for taking close up photographs of what is going on in your mouth. I often get photographs so that you can clearly see what is there and we can work out together what you would like done about it. I can also take \"after\" photographs to show you what has been done. It is also helpful for monitoring things to see if they are stable or deteriorating.")])])
 }]}
 module.exports.render._withStripped = true
 if (false) {
--- resources/assets/js/components/pages/TheBestTechnology/IntraoralCameraComponent.vue
+++ resources/assets/js/components/pages/TheBestTechnology/IntraoralCameraComponent.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h1>Intraoral Camera</h1>
+        <p>The intraoral camera is wonderful for taking close up photographs of what is going on in your mouth. I often get photographs so that you can clearly see what is there and we can work out together what you would like done about it. I can also take "after" photographs to show you what has been done. It is also helpful for monitoring things to see if they are stable or deteriorating.</p>
     </div>
 </template>
 
